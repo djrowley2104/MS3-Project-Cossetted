0b1c8afcf40ccc3970793b70c73036002e9c343e
djrowley2104 <djrowley@hotmail.co.uk>
2020-12-14 19:26:20 +0000

    Data; gaetting the site to communitcate with the data base

--- app.py
+++ app.py
@@ -77,7 +77,21 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("cars.html", username=username)
+    return render_template("classics.html", username=username)
+
+# add new sales task
+@app.route("/sellclassic")
+def add_task():
+    categories = mongo.db.categories.find().sort("Sale_Class", 1)
+    return render_template("sellclassic.html", categories=categories)
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
