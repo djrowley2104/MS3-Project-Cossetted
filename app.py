766e89b56393d350ebec83f2025f474dc3129d4b
djrowley2104 <djrowley@hotmail.co.uk>
2020-12-14 13:03:30 +0000

    Data; getting the register function working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,6 +22,11 @@ mongo = PyMongo(app)
 def get_tasks():
     sale_items = mongo.db.Sale_Item.find()
     return render_template("tasks.html", Sale_Item=sale_items)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 
 
 if __name__ == "__main__":
