ac68f72a7459d6a922b4b6198f34faeab25db48c
djrowley2104 <djrowley@hotmail.co.uk>
2020-12-15 12:30:05 +0000

    Data, getting the site to talk to mongodb

--- app.py
+++ app.py
@@ -98,14 +98,32 @@ def sellclassic():
             "created_by": session["user"]
         }
         mongo.db.Sale_Item.insert_one(task)
-        flash("Task Successfully Added")
+        flash("Classic Successfully Added")
         return redirect(url_for("sellclassic"))
 
     return render_template("sellclassic.html")
 
 # add new sales task for item
-@app.route("/sellitem")
+@app.route("/sellitem", methods=["GET", "POST"])
 def sellitem():
+    if request.method == "POST":
+        approved = "approved" if request.form.get("approved") else "off"
+        task = {
+            "Sellers_Class": request.form.get("Sellers_Class"),
+            "Sellers_Name": request.form.get("Sellers_Name"),
+            "Sellers_Phone_Number": request.form.get("Sellers_Phone_Number"),
+            "Email_Address": request.form.get("Email_Address"),
+            "Item_For_Sale": request.form.get("Item_For_Sale"),
+            "Manufacturer": request.form.get("Manufacturer"),
+            "Model": request.form.get("Model"),
+            "Description_Of_Item": request.form.get("Description_Of_Item"),
+            "Photo": request.form.get("Photo"),
+            "created_by": session["user"]
+        }
+        mongo.db.Sale_Item.insert_one(task)
+        flash("Spares Successfully Added")
+        return redirect(url_for("home.html"))
+
     return render_template("sellitem.html")    
 
 
